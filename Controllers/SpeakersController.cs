7c2f1730b9e205b848479b1aac5c5848b7355615
Wai-Yin Kwan <wykhuh@icloud.com>
2017-02-10 14:04:03 -0800

    add POST speaker

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -14,7 +14,7 @@ namespace CodeCamp.Controllers
     [Route("api/camps/{moniker}/speakers")]
     public class SpeakersController : BaseController
     {
-        private ICampRepository _respository;
+        private ICampRepository _repository;
         private ILogger<SpeakersController> _logger;
         private IMapper _mapper;
 
@@ -22,7 +22,7 @@ namespace CodeCamp.Controllers
             ILogger<SpeakersController> logger,
             IMapper mapper)
         {
-            _respository = repository;
+            _repository = repository;
             _logger = logger;
             _mapper = mapper;
         }
@@ -30,18 +30,44 @@ namespace CodeCamp.Controllers
         [HttpGet]
         public IActionResult Get(string moniker)
         {
-            var speakers = _respository.GetSpeakersByMoniker(moniker);
+            var speakers = _repository.GetSpeakersByMoniker(moniker);
             return Ok(_mapper.Map<IEnumerable<SpeakerModel>>(speakers));
         }
 
         [HttpGet("{id}", Name = "SpeakerGet")]
         public IActionResult Get(string moniker, int id)
         {
-            var speaker = _respository.GetSpeaker(id);
+            var speaker = _repository.GetSpeaker(id);
             if (speaker == null) return NotFound();
             if (speaker.Camp.Moniker != moniker) return BadRequest("Speaker not in specified camp");
 
             return Ok(_mapper.Map<SpeakerModel>(speaker));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string moniker, [FromBody] SpeakerModel model)
+        {
+            try
+            {
+                var camp = _repository.GetCampByMoniker(moniker);
+                if (camp == null) return BadRequest($"could not find camp moniker {moniker}");
+
+                var speaker = _mapper.Map<Speaker>(model);
+                speaker.Camp = camp;
+
+                _repository.Add(speaker);
+
+                if(await _repository.SaveAllAsync())
+                {
+                    var url = Url.Link("SpeakerGet", new { moniker = camp.Moniker, id = speaker.Id });
+                    return Created(url, _mapper.Map<SpeakerModel>(speaker));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"create speaker exception: {ex}");
+            }
+            return BadRequest("create speaker error");
         }
     }
 }
