5555e7d799947a0f92a6f5b0e429fe0aa072f8bb
Wai-Yin Kwan <wykhuh@icloud.com>
2017-02-09 19:56:42 -0800

    add first route and controller

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCamp.Controllers
+{
+    // base route for all the actions in the controler
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        [HttpGet("")]
+        // use IActionResult to return status code with the data 
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Jane", Country = "England" });
+        }
+    }
+}
