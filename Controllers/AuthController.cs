e3ec40eec776a52585199087dd798bbd39084aca
Wai-Yin Kwan <wykhuh@icloud.com>
2017-02-15 08:21:41 -0800

    add endpoint to create token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,6 +6,11 @@ using CodeCamp.Filters;
 using CodeCamp.Models;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace CodeCamp.Controllers
 {
@@ -14,16 +19,22 @@ namespace CodeCamp.Controllers
         private CampContext _context;
         private ILogger<AuthController> _logger;
         private SignInManager<CampUser> _signInManager;
+        private UserManager<CampUser> _userMgr;
+        private IPasswordHasher<CampUser> _hasher;
 
         // allower developers to send us credentials, and we will set a cookie.
         // use cookie if only one browser-based web app uses the api.
         public AuthController(CampContext context, 
             SignInManager<CampUser> signInManager,
-            ILogger<AuthController> logger )
+            ILogger<AuthController> logger,
+            UserManager<CampUser> userMgr,
+            IPasswordHasher<CampUser> hasher)
         {
             _context = context;
             _signInManager = signInManager;
             _logger = logger;
+            _userMgr = userMgr;
+            _hasher = hasher;
         }
 
         // use POST instead of GET because we don't want the crendentials to be sent in plain text query string
@@ -42,13 +53,62 @@ namespace CodeCamp.Controllers
                 }
                 // if login fails, return BadRequest
             }
-            catch (System.Exception)
+            catch (Exception ex)
             {
 
-                _logger.LogError($"Exception thrown while loggin in");
+                _logger.LogError($"Exception thrown while loggin in {ex}");
             }
 
             return BadRequest("Falled login");
+        }
+
+        [ValidateModel]
+        [HttpPost("api/auth/token")]
+        public async Task<IActionResult> CreateToken([FromBody] CredentialModel model)
+        {
+            try
+            {
+                // check if POST username exists 
+                var user = await _userMgr.FindByNameAsync(model.Username);
+                if (user != null)
+                {
+                    // check if POST password matches the stored PasswordHash
+                    if (_hasher.VerifyHashedPassword(user, user.PasswordHash, model.Password)  == PasswordVerificationResult.Success)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                        // in real prod code, but config somewhere else
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ReallyInsecureSecret"));
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        // create token
+                        var token = new JwtSecurityToken(
+                            issuer: "http://localhost",
+                            audience: "http://localhost",
+                            claims: claims,
+                            expires: DateTime.UtcNow.AddMinutes(15),
+                            signingCredentials: creds
+                          );
+
+                        return Ok(new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Exception thrown while creating token: {ex}");
+
+            }
+
+            return BadRequest("Failed token creation");
         }
     }
 }
